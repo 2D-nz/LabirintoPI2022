e8a884b2bcf83388d3d909c71d71b318cd1d7b94
Denis <71805486+2D-nz@users.noreply.github.com>
2022-09-29 21:24:54 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,54 @@
  *
  * @author denis.lsilva9
  */
+
+import java.io.*;
+import java.lang.Thread;
+import java.util.Scanner;
+   
 public class Main {
     
+    public static void main(String[] args) {
+        
+        menu();
+       
+        
+    }
+     public static void menu(){
+        
+        System.out.println("LABIRINTO"); 
+        System.out.println("1 - Jogar");
+        System.out.println("2 - Instruções");
+        System.out.println("3 - Créditos");
+        System.out.println("4 - Sair");
+        Scanner input = new Scanner(System.in);
+        int option = input.nextInt();
+        
+        switch(option){
+            
+            case 2:
+                System.out.println("-----------------");
+                System.out.println("Como jogar:");
+                System.out.println("Serão sempre lhe dadas opções de para onde andar");
+                System.out.println("Dependendo de sorte, você pode ter uma armadilha, ou charada");
+                System.out.println("Charadas podem te dar dicas");
+                System.out.println("Armadilhas podem te ajudar ou atrapalhar");
+                System.out.println("Passa-se de fase, descobrindo o caminho até o final");
+                System.out.println("Ganha-se o jogo, ao passar da última fase");
+                System.out.println("-----------------");
+                menu();
+                
+            case 3:
+                System.out.println("-----------------");
+                System.out.println("Feito por:");
+                System.out.println("Lorena Mafra");
+                System.out.println("Denis Silva");
+                System.out.println("Robson Figueiredo");
+                System.out.println("Simone Colaco");
+                System.out.println("-----------------");
+                menu();
+        }
+        
+        }
 }
+
