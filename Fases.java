b9b27864a4702a2acc4af4bc9e116e061dc54b61
Denis <71805486+2D-nz@users.noreply.github.com>
2022-10-06 23:17:51 +0000

    Update Fases.java

--- Fases.java
+++ Fases.java
@@ -7,32 +7,42 @@ import java.sql.Array;
 
 //
 
+import java.sql.Array;
 import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        int[] positions = new int[]{31, 32, 22, 23, 21};
-        int currentPosition = positions[0];
         move();
 
-
+        
+        
     }
     public static void move() {
-
+        int[] positions = {31, 32, 22, 23, 21};
+        int currentPosition = positions[0];
+        
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Para onde agora?");
         System.out.println("1 - Cima");
         System.out.println("2 - Direita");
         System.out.println("3 - Baixo");
         System.out.println("4 - Esquerda");
-
         int nextPosition = input.nextInt();
 
         switch (nextPosition) {
             case 1:
                 nextPosition = currentPosition + 10;
-                boolean test = Array.asList(positions).contains(nextPosition);
-                System.out.println(test);
+                boolean val = contains(positions, currentPosition);
+                if(val == true){
+                    System.out.println("foi");
+                    
+                } else {System.out.println("Não há caminho por aqui");
+                    
+                }
+                
+                
+                
 
         }
     }
