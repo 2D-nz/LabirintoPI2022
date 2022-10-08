29c20afdbb2bb3f066104723b8382184102ca487
lorenamafra <108240782+lorenamafra@users.noreply.github.com>
2022-10-08 10:37:42 -0300

    Update Charadas.java

--- Charadas.java
+++ Charadas.java
@@ -7,12 +7,35 @@
  *
  * @author denis.lsilva9
  */
+package charadas;
+
 import java.util.Scanner;
-public class Charadas {
-    public static void main(String[] args) {
-}
-    Scanner input = new Scanner (System.in);
-    
-    int opcao;
-    
-    System.out.println(");
+
+public class fase_charada {
+
+	private static final String String = null;
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+
+		String charada = null;
+		int i = 0;
+
+		do {
+			System.out.println("O que é, o que é: É feito de água, mas se for colocado dentro da água morrerá.");
+			System.out.println("Responda:\n");
+			charada = input.next();
+			charada = charada.toLowerCase();
+
+			if(charada.equals("gelo") == true){
+				System.out.println("Passou para a próxima fase!");
+				i = 4;}
+			else{
+				i++;
+				System.out.println("Resposta errada. Você ainda tem " + (3-i) + " tentativas");
+				if (i == 3) {
+					System.out.println("Você perdeu!");
+				}
+			} 
+		} while (i < 3);
+	} 
