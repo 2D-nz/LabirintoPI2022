dfe4ed36b20686f76095ab5475fa4bba95427f82
Denis <71805486+2D-nz@users.noreply.github.com>
2022-11-02 23:46:30 -0300

    Create Tutorial.java

---
+++ Tutorial.java
@@ -0,0 +1,23 @@
+public class Tutorial {
+    public static void main() {
+        System.out.println("---------------");
+        System.out.println("Bem vindo(a/e) ao tutorial!");
+        System.out.println("Neste jogo você está preso em um labirinto!");
+        System.out.println("Pense nisso como um jogo da velha:");
+        System.out.println(" * |   |  ");
+        System.out.println(" * |   | * ");
+        System.out.println(" x |   | * ");
+
+
+        System.out.println("O pequeno x é você");
+        System.out.println("Durante o jogo você poderá se movimentar em uma das quatro direções");
+        System.out.println("E assim deve achar o final do labirinto, neste caso representado pelo o");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Neste caso, você deve ir para a direita, para cima duas vezes e depois para a direita de novo");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Durante as fases você será surpreendido por charadas");
+        System.out.println("Se acertar, você conseguirá dicas de como avançar!");
+        System.out.println("Se conseguir achar o mapa da fase, ficará mais fácil de você achar o caminho");
+        System.out.println("Boa sorte! :)");
+    }
+}
