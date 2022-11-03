953850f2bfe8461f7c2872b0c2ca30af058fba1e
Denis <71805486+2D-nz@users.noreply.github.com>
2022-11-02 23:41:46 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,57 @@
-package com.mycompany.mavenproject1;
 import java.util.Scanner;
 public class Main {
-    
+
     public static void jogo(){
-        int retornoFase = 0;
-       Fase1 fase1 = new Fase1();
-       retornoFase = fase1.execFase1();
-       if(retornoFase==2){
-           Fase2 fase2 = new Fase2();
-           
-       }
-       int contFase = 1;
+        Tutorial.main();
+        System.out.println("Este foi o fim do tutorial! Boa sorte");
+        System.out.println("--------------------------------------");
+        //int faseIndex = Fase1.main();
+        //execFase(faseIndex);
+        int faseIndex = Fase2.main();
+        execFase(faseIndex);
+        //faseIndex = Fase3.main();
+        //exeFase(faseIndex);
+
+
+
     }
-    
+
+    public static void gameOver (){
+            System.out.println("----------------------");
+            System.out.println("Game Over");
+            System.exit(0);
+        }
+
+
     public static void main(String[] args) {
         menu();
         jogo();
-        
-        
-        
+
+
+
     }
-     public static void menu(){
-        
-        System.out.println("LABIRINTO"); 
+
+    public static void execFase(int faseIndex){
+        switch (faseIndex){
+            case 2:
+                Fase2.main();
+                break;
+            case 3:
+                //Fase3.main();
+                break;
+            case 4:
+                //Fase4.main;
+                break;
+            case 5:
+                //Fase5.main();
+                break;
+            default:
+                gameOver();
+        }
+    }
+    public static void menu(){
+
+        System.out.println("LABIRINTO");
         System.out.println("1 - Jogar");
         System.out.println("2 - Instruções");
         System.out.println("3 - Créditos");
@@ -31,38 +60,37 @@ public class Main {
         int option = input.nextInt();
         int i = 0;
         while (i != 10){
-        switch(option){
-            
-            case 1:
-                jogo();
-                i = 10;
-                break;
-            
-            case 2:
-                System.out.println("-----------------");
-                System.out.println("Como jogar:");
-                System.out.println("Serão sempre lhe dadas opções de para onde andar");
-                System.out.println("Dependendo de sorte, você pode ter uma armadilha, ou charada");
-                System.out.println("Charadas podem te dar dicas");
-                System.out.println("Passa-se de fase, descobrindo o caminho até o final");
-                System.out.println("Ganha-se o jogo, ao passar da última fase");
-                System.out.println("-----------------");
-                menu();
-                
-            case 3:
-                System.out.println("-----------------");
-                System.out.println("Feito por:");
-                System.out.println("Lorena Mafra");
-                System.out.println("Denis Silva");
-                System.out.println("Simone Colaco");
-                System.out.println("-----------------");
-                menu();
-                
-            case 4:
-                System.out.println("Fim do programa!");
-                i = 10;
+            switch(option){
+
+                case 1:
+                    i = 10;
+                    break;
+
+                case 2:
+                    System.out.println("-----------------");
+                    System.out.println("Como jogar:");
+                    System.out.println("Serão sempre lhe dadas opções de para onde andar");
+                    System.out.println("Dependendo de sorte, você pode ter uma armadilha, ou charada");
+                    System.out.println("Charadas podem te dar dicas");
+                    System.out.println("Passa-se de fase, descobrindo o caminho até o final");
+                    System.out.println("Ganha-se o jogo, ao passar da última fase");
+                    System.out.println("-----------------");
+                    menu();
+
+                case 3:
+                    System.out.println("-----------------");
+                    System.out.println("Feito por:");
+                    System.out.println("Lorena Mafra");
+                    System.out.println("Denis Silva");
+                    System.out.println("Simone Colaco");
+                    System.out.println("-----------------");
+                    menu();
+
+                case 4:
+                    System.out.println("Fim do programa!");
+                    i = 10;
+            }
+
         }
-        
-        }
-     }
+    }
 }
