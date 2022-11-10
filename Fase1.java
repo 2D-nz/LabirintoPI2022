05662010bb8c74a0e194dab69171e3bee5b2cf5a
Denis <71805486+2D-nz@users.noreply.github.com>
2022-11-10 19:12:16 -0300

    Update Fase1.java

--- Fase1.java
+++ Fase1.java
@@ -1,15 +1,4 @@
-import java.sql.Array;
-
-
-// 1 - Definir fase
-// 2- Tornar interativo
-// 3- Adc Charadas e Armadilhas
-
-//
-
-import java.sql.Array;
-import java.util.Scanner;
-import java.util.Arrays;import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Fase1 {
@@ -59,7 +48,7 @@ public class Fase1 {
                     if(pos == fase1.get(3)){
                         System.out.println("Mapa da fase:");
                         System.out.println(" X | X |   |");
-                        System.out.println(" X |   |   |");
+                        System.out.println(" X |   | @ |");
                         System.out.println("   |   | X |");
                         System.out.println("Boa sorte!");
                         i=50;
@@ -77,45 +66,8 @@ public class Fase1 {
             }
         }
         System.out.println("Parabéns, você passou de fase");
-        return 1;
+        System.out.println("------------------------------");
+        return 2;
 
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        move();
-
-        
-        
-    }
-    public static void move() {
-        int[] positions = {31, 32, 22, 23, 21};
-        int currentPosition = positions[0];
-        
-        
-        Scanner input = new Scanner(System.in);
-        System.out.println("Para onde agora?");
-        System.out.println("1 - Cima");
-        System.out.println("2 - Direita");
-        System.out.println("3 - Baixo");
-        System.out.println("4 - Esquerda");
-        int nextPosition = input.nextInt();
-
-        switch (nextPosition) {
-            case 1:
-                nextPosition = currentPosition + 10;
-                boolean val = contains(positions, currentPosition);
-                if(val == true){
-                    System.out.println("foi");
-                    
-                } else {System.out.println("Não há caminho por aqui");
-                    
-                }
-                
-                
-                
-
-        }
     }
 }
